98e8eab7bb6f12bac76a0bc1fa9b2179581493fa
proxydyzed <tonnoysarker2@gmail.com>
2026-01-27 02:55:26 +0600

    export entries

---
+++ export.js
@@ -0,0 +1,27 @@
+export {
+  Vector3,
+  Vector3Uint8,
+  Vector3Uint16,
+  Vector3Float,
+
+  Blocks,
+
+  GameData,
+  Prefab,
+  Setting,
+  Connection,
+
+  decode,
+  encode,
+  unlock,
+
+  compressBuffer,
+  decompressBuffer,
+
+  iterAsWorld,
+  SparseTiles,
+} from "./lib/export.js";
+
+export {
+  FancadeGame,
+} from "./unstable/fancade-game.js";
