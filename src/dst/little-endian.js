3958395ba3412632fa31663e78e9af071893c697
proxydyzed <tonnoysarker2@gmail.com>
2026-01-27 02:54:17 +0600

    refactor

--- src/dst/little-endian.js
+++
@@ -1,5 +0,0 @@
-// source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/DataView#endianness
-const arrayBuffer = new ArrayBuffer(2);
-void new DataView(arrayBuffer).setInt16(0, 256, true /* little endian */);
-// Int16Array uses the platform's endianness.
-export const littleEndian = new Int16Array(arrayBuffer)[0] === 256;
--- src/dst/object-to-reverse-map.js
+++
@@ -1,3 +0,0 @@
-export function objectToReverseMap(object) {
-  return new Map(Object.entries(object).map(([key, value]) => ([value, key])));
-}
