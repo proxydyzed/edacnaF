54bc3bfedb5a87b7489715c22d4c75df4c2ebe39
proxydyzed <tonnoysarker2@gmail.com>
2026-05-26 14:37:36 +0600

    minor changes to the enclosure

--- utils/little-endian.js
+++ utils/little-endian.js
@@ -1,5 +1,8 @@
-// source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/DataView#endianness
-const arrayBuffer = new ArrayBuffer(2);
-void new DataView(arrayBuffer).setInt16(0, 256, true /* little endian */);
-// Int16Array uses the platform's endianness.
-export const littleEndian = new Int16Array(arrayBuffer)[0] === 256;
+export const littleEndian = (function() {
+  // source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/DataView#endianness
+  const arrayBuffer = new ArrayBuffer(2);
+  void new DataView(arrayBuffer).setInt16(0, 256, true /* little endian */);
+
+  // Int16Array uses the platform's endianness.
+  return new Int16Array(arrayBuffer)[0] === 256;
+})();
